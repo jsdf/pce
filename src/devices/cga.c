c215bf4fc1eef5e950dcc0885bee1c15917b1123
Hampa Hug <hampa@hampa.ch>
2003-11-17 23:57:21 +0000

    Format string fix.

--- src/devices/cga.c
+++ src/devices/cga.c
@@ -5,7 +5,7 @@
 /*****************************************************************************
  * File name:     cga.c                                                      *
  * Created:       2003-04-18 by Hampa Hug <hampa@hampa.ch>                   *
- * Last modified: 2003-11-16 by Hampa Hug <hampa@hampa.ch>                   *
+ * Last modified: 2003-11-17 by Hampa Hug <hampa@hampa.ch>                   *
  * Copyright:     (C) 2003 by Hampa Hug <hampa@hampa.ch>                     *
  *****************************************************************************/
 
@@ -20,7 +20,7 @@
  * Public License for more details.                                          *
  *****************************************************************************/
 
-/* $Id: cga.c,v 1.2 2003/11/16 05:05:28 hampa Exp $ */
+/* $Id: cga.c,v 1.3 2003/11/17 23:57:21 hampa Exp $ */
 
 
 #include <stdio.h>
@@ -90,7 +90,7 @@ video_t *cga_new (terminal_t *trm, ini_sct_t *sct)
 
   memsize = (memsize < 16384) ? 16384 : memsize;
 
-  pce_log (MSG_INF, "video:\tCGA io=0x%04x membase=0x%05x memsize=0x%05x\n",
+  pce_log (MSG_INF, "video:\tCGA io=0x%04lx membase=0x%05lx memsize=0x%05lx\n",
     iobase, membase, memsize
   );
 
