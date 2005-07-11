054360a546d287b937b7653a775d01c3308b7473
Hampa Hug <hampa@hampa.ch>
2005-07-11 07:44:59 +0000

    Added a new stdint.h header for systems that don't have it.

---
+++ src/lib/stdint/stdint.h
@@ -0,0 +1,42 @@
+/*****************************************************************************
+ * pce                                                                       *
+ *****************************************************************************/
+
+/*****************************************************************************
+ * File name:     src/lib/stdint/stdint.h                                    *
+ * Created:       2005-07-11 by Hampa Hug <hampa@hampa.ch>                   *
+ * Last modified: 2005-07-11 by Hampa Hug <hampa@hampa.ch>                   *
+ * Copyright:     (C) 2005 Hampa Hug <hampa@hampa.ch>                        *
+ *****************************************************************************/
+
+/*****************************************************************************
+ * This program is free software. You can redistribute it and / or modify it *
+ * under the terms of the GNU General Public License version 2 as  published *
+ * by the Free Software Foundation.                                          *
+ *                                                                           *
+ * This program is distributed in the hope  that  it  will  be  useful,  but *
+ * WITHOUT  ANY   WARRANTY,   without   even   the   implied   warranty   of *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU  General *
+ * Public License for more details.                                          *
+ *****************************************************************************/
+
+/* $Id$ */
+
+
+#ifndef PCE_LIB_STDINT_H
+#define PCE_LIB_STDINT_H 1
+
+
+#include <config.h>
+
+
+#ifdef HAVE_INTTYPES_H
+#include <inttypes.h>
+#else
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned long uint32_t;
+#endif
+
+
+#endif
