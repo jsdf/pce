cc1d0188c7a181878680e265756be471c0a37030
Hampa Hug <hampa@hampa.ch>
2005-11-29 13:50:04 +0000

    Renamed directory apps to utils.

---
+++ src/utils/pceimg/pceimg.h
@@ -0,0 +1,23 @@
+/*****************************************************************************
+ * pce                                                                       *
+ *****************************************************************************/
+
+/*****************************************************************************
+ * File name:     src/apps/pceimg/pceimg.h                                   *
+ * Created:       2005-11-29 by Hampa Hug <hampa@hampa.ch>                   *
+ * Last modified: 2005-11-29 by Hampa Hug <hampa@hampa.ch>                   *
+ * Copyright:     (C) 2005 Hampa Hug <hampa@hampa.ch>                        *
+ *****************************************************************************/
+
+/*****************************************************************************
+ * This program is free software. You can redistribute it and / or modify it *
+ * under the terms of the GNU General Public License version 2 as  published *
+ * by the Free Software Foundation.                                          *
+ *                                                                           *
+ * This program is distributed in the hope  that  it  will  be  useful,  but *
+ * WITHOUT  ANY   WARRANTY,   without   even   the   implied   warranty   of *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU  General *
+ * Public License for more details.                                          *
+ *****************************************************************************/
+
+/* $Id$ */
