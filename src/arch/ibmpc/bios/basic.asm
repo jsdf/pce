336816a936c9cf9fb1f8c32a9617aa3ec4ef0340
Hampa Hug <hampa@hampa.ch>
2008-01-23 01:02:00 +0000

    [ibmpc] Fix BIOS copyright and license

--- src/arch/ibmpc/bios/basic.asm
+++ src/arch/ibmpc/bios/basic.asm
@@ -5,19 +5,6 @@
 ;*****************************************************************************
 ;* File name:     src/arch/ibmpc/bios/basic.asm                              *
 ;* Created:       2003-04-14 by Hampa Hug <hampa@hampa.ch>                   *
-;* Last modified: 2004-01-10 by Hampa Hug <hampa@hampa.ch>                   *
-;* Copyright:     (C) 2003-2004 by Hampa Hug <hampa@hampa.ch>                *
-;*****************************************************************************
-
-;*****************************************************************************
-;* This program is free software. You can redistribute it and / or modify it *
-;* under the terms of the GNU General Public License version 2 as  published *
-;* by the Free Software Foundation.                                          *
-;*                                                                           *
-;* This program is distributed in the hope  that  it  will  be  useful,  but *
-;* WITHOUT  ANY   WARRANTY,   without   even   the   implied   warranty   of *
-;* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU  General *
-;* Public License for more details.                                          *
 ;*****************************************************************************
 
 ; $Id$
--- src/arch/ibmpc/bios/bios.asm
+++ src/arch/ibmpc/bios/bios.asm
@@ -5,18 +5,6 @@
 ;*****************************************************************************
 ;* File name:     src/arch/ibmpc/bios/bios.asm                               *
 ;* Created:       2003-04-14 by Hampa Hug <hampa@hampa.ch>                   *
-;* Copyright:     (C) 2003-2007 Hampa Hug <hampa@hampa.ch>                   *
-;*****************************************************************************
-
-;*****************************************************************************
-;* This program is free software. You can redistribute it and / or modify it *
-;* under the terms of the GNU General Public License version 2 as  published *
-;* by the Free Software Foundation.                                          *
-;*                                                                           *
-;* This program is distributed in the hope  that  it  will  be  useful,  but *
-;* WITHOUT  ANY   WARRANTY,   without   even   the   implied   warranty   of *
-;* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU  General *
-;* Public License for more details.                                          *
 ;*****************************************************************************
 
 ; $Id$
