0f770259c9a3329a65b74bc19829407d864d8fce
Hampa Hug <hampa@hampa.ch>
2010-11-01 21:55:23 +0100

    ibmpc: Don't patch int 19 more than once

--- src/arch/ibmpc/cmd.c
+++ src/arch/ibmpc/cmd.c
@@ -508,6 +508,14 @@ void pce_op_int (void *ext, unsigned char n)
 			return;
 		}
 
+		if (pc->patch_bios_init) {
+			return;
+		}
+
+		if (e86_get_mem16 (pc->cpu, 0, 4 * 0x19 + 2) < 0xf000) {
+			return;
+		}
+
 		seg = pc_get_pcex_seg (pc);
 
 		if (seg == 0) {
