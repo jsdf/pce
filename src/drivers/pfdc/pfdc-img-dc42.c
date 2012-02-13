4b1f4507098b2d9d608df5d33c2592b593f5ef4e
Hampa Hug <hampa@hampa.ch>
2012-02-13 18:20:51 +0100

    pfdc/dc42: Fix loading and saving images when the sectors are not in order

--- src/drivers/pfdc/pfdc-img-dc42.c
+++ src/drivers/pfdc/pfdc-img-dc42.c
@@ -162,7 +162,11 @@ int dc42_load_tags (FILE *fp, pfdc_img_t *img, unsigned long size, unsigned long
 			trk = cyl->trk[h];
 
 			for (s = 0; s < trk->sct_cnt; s++) {
-				sct = trk->sct[s];
+				sct = pfdc_img_get_sector (img, c, h, s, 0);
+
+				if (sct == NULL) {
+					return (1);
+				}
 
 				if (size < 12) {
 					return (1);
@@ -282,16 +286,110 @@ pfdc_img_t *pfdc_load_dc42 (FILE *fp)
 	return (img);
 }
 
+
+/*
+ * Check if an image is a 400K or 800K GCR disk
+ */
 static
-int dc42_save_data (FILE *fp, const pfdc_img_t *img, int gcr)
+int dc42_check_gcr (const pfdc_img_t *img, unsigned trk_cnt)
 {
-	unsigned         c, h, s, si;
-	pfdc_img_t       *tmp;
+	unsigned         c, h, s, sct_cnt;
 	const pfdc_cyl_t *cyl;
 	const pfdc_trk_t *trk;
 	const pfdc_sct_t *sct;
 
-	tmp = (pfdc_img_t *) img;
+	if (img->cyl_cnt != 80) {
+		return (0);
+	}
+
+	for (c = 0; c < img->cyl_cnt; c++) {
+		cyl = img->cyl[c];
+
+		if (cyl->trk_cnt != trk_cnt) {
+			return (0);
+		}
+
+		sct_cnt = 12 - c / 16;
+
+		for (h = 0; h < trk_cnt; h++) {
+			trk = cyl->trk[h];
+
+			if (trk->sct_cnt != sct_cnt) {
+				return (0);
+			}
+
+			for (s = 0; s < trk->sct_cnt; s++) {
+				sct = trk->sct[s];
+
+				if (sct->n != 512) {
+					return (0);
+				}
+
+				if (sct->s >= sct_cnt) {
+					return (0);
+				}
+			}
+		}
+	}
+
+	return (1);
+}
+
+/*
+ * Check if an image is a 720K or 1440K MFM disk
+ */
+static
+int dc42_check_mfm (const pfdc_img_t *img, unsigned trk_cnt, unsigned sct_cnt)
+{
+	unsigned         c, h, s;
+	const pfdc_cyl_t *cyl;
+	const pfdc_trk_t *trk;
+	const pfdc_sct_t *sct;
+
+	if (img->cyl_cnt != 80) {
+		return (0);
+	}
+
+	for (c = 0; c < img->cyl_cnt; c++) {
+		cyl = img->cyl[c];
+
+		if (cyl->trk_cnt != trk_cnt) {
+			return (0);
+		}
+
+		for (h = 0; h < trk_cnt; h++) {
+			trk = cyl->trk[h];
+
+			if (trk->sct_cnt != sct_cnt) {
+				return (0);
+			}
+
+			for (s = 0; s < trk->sct_cnt; s++) {
+				sct = trk->sct[s];
+
+				if (sct->n != 512) {
+					return (0);
+				}
+
+				if ((sct->s < 1) || (sct->s > sct_cnt)) {
+					return (0);
+				}
+			}
+		}
+	}
+
+	return (1);
+}
+
+/*
+ * Check if an image contains any tags
+ */
+static
+int dc42_check_tags (const pfdc_img_t *img)
+{
+	unsigned         c, h, s;
+	const pfdc_cyl_t *cyl;
+	const pfdc_trk_t *trk;
 
 	for (c = 0; c < img->cyl_cnt; c++) {
 		cyl = img->cyl[c];
@@ -300,15 +398,7 @@ int dc42_save_data (FILE *fp, const pfdc_img_t *img, int gcr)
 			trk = cyl->trk[h];
 
 			for (s = 0; s < trk->sct_cnt; s++) {
-				si = gcr ? s : (s + 1);
-
-				sct = pfdc_img_get_sector (tmp, c, h, si, 0);
-
-				if (sct == NULL) {
-					return (1);
-				}
-
-				if (pfdc_write (fp, sct->data, 512)) {
+				if (trk->sct[s]->tag_cnt > 0) {
 					return (1);
 				}
 			}
@@ -319,27 +409,95 @@ int dc42_save_data (FILE *fp, const pfdc_img_t *img, int gcr)
 }
 
 static
-int dc42_save_tags (FILE *fp, const pfdc_img_t *img, int gcr)
+int dc42_calc_checksums (const pfdc_img_t *img, unsigned cn, unsigned hn, unsigned sn,
+	unsigned long *data, unsigned long *tags)
 {
-	unsigned         c, h, s, si;
+	unsigned         c, h, s, s1, s2;
 	unsigned char    buf[12];
 	pfdc_img_t       *tmp;
-	const pfdc_cyl_t *cyl;
-	const pfdc_trk_t *trk;
+	const pfdc_sct_t *sct;
+
+	*data = 0;
+	*tags = 0;
+
+	tmp = (pfdc_img_t *) img;
+
+	s1 = (sn == 0) ? 0 : 1;
+
+	for (c = 0; c < cn; c++) {
+		s2 = (sn == 0) ? (12 - c / 16) : (sn + 1);
+
+		for (h = 0; h < hn; h++) {
+			for (s = s1; s < s2; s++) {
+				sct = pfdc_img_get_sector (tmp, c, h, s, 0);
+
+				if (sct == NULL) {
+					return (1);
+				}
+
+				*data = dc42_calc_checksum (sct->data, sct->n, *data);
+
+				if ((c > 0) || (h > 0) || (s > 0)) {
+					pfdc_sct_get_tags (sct, buf, 12);
+					*tags = dc42_calc_checksum (buf, 12, *tags);
+				}
+			}
+		}
+	}
+
+	return (0);
+}
+
+static
+int dc42_save_data (FILE *fp, const pfdc_img_t *img, unsigned cn, unsigned hn, unsigned sn)
+{
+	unsigned         c, h, s, s1, s2;
+	pfdc_img_t       *tmp;
 	const pfdc_sct_t *sct;
 
 	tmp = (pfdc_img_t *) img;
 
-	for (c = 0; c < img->cyl_cnt; c++) {
-		cyl = img->cyl[c];
+	s1 = (sn == 0) ? 0 : 1;
 
-		for (h = 0; h < cyl->trk_cnt; h++) {
-			trk = cyl->trk[h];
+	for (c = 0; c < cn; c++) {
+		s2 = (sn == 0) ? (12 - c / 16) : (sn + 1);
 
-			for (s = 0; s < trk->sct_cnt; s++) {
-				si = gcr ? s : (s + 1);
+		for (h = 0; h < hn; h++) {
+			for (s = s1; s < s2; s++) {
+				sct = pfdc_img_get_sector (tmp, c, h, s, 0);
 
-				sct = pfdc_img_get_sector (tmp, c, h, si, 0);
+				if (sct == NULL) {
+					return (1);
+				}
+
+				if (pfdc_write (fp, sct->data, sct->n)) {
+					return (1);
+				}
+			}
+		}
+	}
+
+	return (0);
+}
+
+static
+int dc42_save_tags (FILE *fp, const pfdc_img_t *img, unsigned cn, unsigned hn, unsigned sn)
+{
+	unsigned         c, h, s, s1, s2;
+	unsigned char    buf[12];
+	pfdc_img_t       *tmp;
+	const pfdc_sct_t *sct;
+
+	tmp = (pfdc_img_t *) img;
+
+	s1 = (sn == 0) ? 0 : 1;
+
+	for (c = 0; c < cn; c++) {
+		s2 = (sn == 0) ? (12 - c / 16) : (sn + 1);
+
+		for (h = 0; h < hn; h++) {
+			for (s = s1; s < s2; s++) {
+				sct = pfdc_img_get_sector (tmp, c, h, s, 0);
 
 				if (sct == NULL) {
 					return (1);
@@ -358,7 +516,7 @@ int dc42_save_tags (FILE *fp, const pfdc_img_t *img, int gcr)
 }
 
 static
-int dc42_set_image_name (unsigned char *buf, const pfdc_img_t *img)
+int dc42_set_image_name (unsigned char *buf, const pfdc_img_t *img, unsigned mdb_sct)
 {
 	unsigned            i, n;
 	const               pfdc_sct_t *sct;
@@ -366,7 +524,7 @@ int dc42_set_image_name (unsigned char *buf, const pfdc_img_t *img)
 
 	src = (const unsigned char *) "\x06Noname";
 
-	sct = pfdc_img_get_sector ((pfdc_img_t *) img, 0, 0, 2, 0);
+	sct = pfdc_img_get_sector ((pfdc_img_t *) img, 0, 0, mdb_sct, 0);
 
 	if (sct != NULL) {
 		if ((sct->data[0] == 'B') && (sct->data[1] == 'D')) {
@@ -394,93 +552,61 @@ int dc42_set_image_name (unsigned char *buf, const pfdc_img_t *img)
 
 int pfdc_save_dc42 (FILE *fp, const pfdc_img_t *img)
 {
-	int              tags, gcr;
-	unsigned         c, h, s;
-	unsigned         enc;
-	unsigned long    data_check, tags_check;
-	unsigned long    data_size, tags_size;
-	unsigned char    buf[128];
-	const pfdc_cyl_t *cyl;
-	const pfdc_trk_t *trk;
-	const pfdc_sct_t *sct;
+	unsigned      cn, hn, sn, enc;
+	unsigned long data_check, tags_check;
+	unsigned long data_size, tags_size;
+	unsigned char buf[128];
 
-	data_check = 0;
-	tags_check = 0;
+	cn = 80;
+	hn = 2;
+	sn = 0;
 
-	data_size = 0;
+	tags_size = 0;
 
-	tags = 0;
+	if (dc42_check_gcr (img, 1)) {
+		hn = 1;
+		enc = 0x0002;
+		data_size = 400UL * 1024UL;
 
-	for (c = 0; c < img->cyl_cnt; c++) {
-		cyl = img->cyl[c];
-
-		for (h = 0; h < cyl->trk_cnt; h++) {
-			trk = cyl->trk[h];
-
-			for (s = 0; s < trk->sct_cnt; s++) {
-				sct = trk->sct[s];
-
-				if (sct->n != 512) {
-					return (1);
-				}
-
-				data_check = dc42_calc_checksum (sct->data, sct->n, data_check);
-				data_size += 512;
-
-				if (sct->tag_cnt > 0) {
-					tags = 1;
-				}
-
-				pfdc_sct_get_tags (sct, buf, 12);
-
-				if ((c > 0) || (h > 0) || (s > 0)) {
-					tags_check = dc42_calc_checksum (buf, 12, tags_check);
-				}
-			}
+		if (dc42_check_tags (img)) {
+			tags_size = 12 * (data_size / 512);
 		}
 	}
+	else if (dc42_check_gcr (img, 2)) {
+		enc = 0x0122;
+		data_size = 800UL * 1024UL;
 
-	if (tags) {
-		tags_size = 12 * (data_size / 512);
+		if (dc42_check_tags (img)) {
+			tags_size = 12 * (data_size / 512);
+		}
+	}
+	else if (dc42_check_mfm (img, 2, 9)) {
+		sn = 9;
+		enc = 0x0222;
+		data_size = 720UL * 1024UL;
+	}
+	else if (dc42_check_mfm (img, 2, 18)) {
+		sn = 18;
+		enc = 0x0322;
+		data_size = 1440UL * 1024UL;
 	}
 	else {
-		tags_size = 0;
+		return (1);
+	}
+
+	if (dc42_calc_checksums (img, cn, hn, sn, &data_check, &tags_check)) {
+		return (1);
 	}
 
 	memset (buf, 0, 128);
 
-	dc42_set_image_name (buf, img);
+	dc42_set_image_name (buf, img, (sn == 0) ? 2 : 3);
 
 	pfdc_set_uint32_be (buf, 64, data_size);
 	pfdc_set_uint32_be (buf, 68, tags_size);
 
 	pfdc_set_uint32_be (buf, 72, data_check);
 	pfdc_set_uint32_be (buf, 76, tags_check);
-
-	switch (data_size) {
-	case 400UL * 1024UL:
-		enc = 0x0002;
-		gcr = 1;
-		break;
-
-	case 800UL * 1024UL:
-		enc = 0x0122;
-		gcr = 1;
-		break;
-
-	case 720UL * 1024UL:
-		enc = 0x0222;
-		gcr = 0;
-		break;
-
-	case 1440UL * 1024UL:
-		enc = 0x0322;
-		gcr = 0;
-		break;
-
-	default:
-		return (1);
-	}
 
 	pfdc_set_uint16_be (buf, 80, enc);
 	pfdc_set_uint16_be (buf, 82, 0x0100);
@@ -489,12 +615,12 @@ int pfdc_save_dc42 (FILE *fp, const pfdc_img_t *img)
 		return (1);
 	}
 
-	if (dc42_save_data (fp, img, gcr)) {
+	if (dc42_save_data (fp, img, cn, hn, sn)) {
 		return (1);
 	}
 
-	if (tags) {
-		if (dc42_save_tags (fp, img, gcr)) {
+	if (tags_size > 0) {
+		if (dc42_save_tags (fp, img, cn, hn, sn)) {
 			return (1);
 		}
 	}
