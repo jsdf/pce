7ccbd5d56d7b82e8bcba0e6a3e3574f1121caa6f
Hampa Hug <hampa@hampa.ch>
2014-01-19 00:59:13 +0100

    pri: Handle ID over index when decoding MFM/FM

--- src/drivers/pri/mfm-ibm.c
+++ src/drivers/pri/mfm-ibm.c
@@ -5,7 +5,7 @@
 /*****************************************************************************
  * File name:   src/drivers/pri/mfm-ibm.c                                    *
  * Created:     2012-02-01 by Hampa Hug <hampa@hampa.ch>                     *
- * Copyright:   (C) 2012-2013 Hampa Hug <hampa@hampa.ch>                     *
+ * Copyright:   (C) 2012-2014 Hampa Hug <hampa@hampa.ch>                     *
  *****************************************************************************/
 
 /*****************************************************************************
@@ -398,7 +398,7 @@ psi_trk_t *pri_decode_mfm_trk (pri_trk_t *trk, unsigned h, pri_dec_mfm_t *par)
 				break;
 			}
 
-			if (trk->wrap) {
+			if ((trk->wrap) && (trk->idx >= (4 * 16))) {
 				break;
 			}
 
@@ -421,7 +421,7 @@ psi_trk_t *pri_decode_mfm_trk (pri_trk_t *trk, unsigned h, pri_dec_mfm_t *par)
 				break;
 			}
 
-			if (trk->wrap) {
+			if ((trk->wrap) && (trk->idx >= 16)) {
 				break;
 			}
 
