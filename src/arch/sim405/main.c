50740b40073c35700049fec2861cf680d81a2f11
Hampa Hug <hampa@hampa.ch>
2015-10-01 13:21:55 +0200

    sim405: Use the new command line argument parsing

--- src/arch/sim405/main.c
+++ src/arch/sim405/main.c
@@ -5,7 +5,7 @@
 /*****************************************************************************
  * File name:   src/arch/sim405/main.c                                       *
  * Created:     2004-06-01 by Hampa Hug <hampa@hampa.ch>                     *
- * Copyright:   (C) 2004-2013 Hampa Hug <hampa@hampa.ch>                     *
+ * Copyright:   (C) 2004-2015 Hampa Hug <hampa@hampa.ch>                     *
  * Copyright:   (C) 2004-2006 Lukas Ruf <ruf@lpr.ch>                         *
  *****************************************************************************/
 
@@ -26,39 +26,65 @@
  *****************************************************************************/
 
 
-#include <config.h>
+#include "main.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-
 #include <signal.h>
 
-#include "main.h"
+#include <lib/getopt.h>
+#include <lib/path.h>
 
 
-char      *par_cpu = NULL;
+static pce_option_t opts[] = {
+	{ '?', 0, "help", NULL, "Print usage information" },
+	{ 'c', 1, "config", "string", "Set the config file name [none]" },
+	{ 'd', 1, "path", "string", "Add a directory to the search path" },
+	{ 'i', 1, "ini-prefix", "string", "Add an ini string before the config file" },
+	{ 'I', 1, "ini-append", "string", "Add an ini string after the config file" },
+	{ 'l', 1, "log", "string", "Set the log file name [none]" },
+	{ 'p', 1, "cpu", "string", "Set the CPU model" },
+	{ 'q', 0, "quiet", NULL, "Set the log level to error [no]" },
+	{ 'r', 0, "run", NULL, "Start running immediately [no]" },
+	{ 'v', 0, "verbose", NULL, "Set the log level to debug [no]" },
+	{ 'V', 0, "version", NULL, "Print version information" },
+	{  -1, 0, NULL, NULL, NULL }
+};
 
-unsigned  par_xlat = P405_XLAT_CPU;
 
-sim405_t  *par_sim = NULL;
+char                 *par_cpu = NULL;
 
-unsigned  par_sig_int = 0;
+unsigned             par_xlat = P405_XLAT_CPU;
 
-ini_sct_t *par_cfg = NULL;
+sim405_t             *par_sim = NULL;
+
+unsigned             par_sig_int = 0;
+
+ini_sct_t            *par_cfg = NULL;
+
+static ini_strings_t par_ini_str;
 
 
 static
-void prt_help (void)
+void print_help (void)
+{
+	pce_getopt_help (
+		"pce-sim405: PowerPC 405 emulator",
+		"usage: pce-sim405 [options]",
+		opts
+	);
+
+	fflush (stdout);
+}
+
+static
+void print_version (void)
 {
 	fputs (
-		"usage: pce-sim405 [options]\n"
-		"  -c, --config string    Set the config file\n"
-		"  -l, --log string       Set the log file\n"
-		"  -p, --cpu string       Set the cpu model\n"
-		"  -q, --quiet            Quiet operation [no]\n"
-		"  -r, --run              Start running immediately\n"
-		"  -v, --verbose          Verbose operation [no]\n",
+		"pce-sim405 version " PCE_VERSION_STR
+		"\n\n"
+		"Copyright (C) 2004-2015 Hampa Hug <hampa@hampa.ch>\n",
 		stdout
 	);
 
@@ -66,16 +92,12 @@ void prt_help (void)
 }
 
 static
-void prt_version (void)
+void s405_log_banner (void)
 {
-	fputs (
-		"pce-sim405 version " PCE_VERSION_STR
-		"\n\n"
-		"Copyright (C) 1995-2012 Hampa Hug <hampa@hampa.ch>\n",
-		stdout
+	pce_log (MSG_MSG,
+		"pce-sim405 version " PCE_VERSION_STR "\n"
+		"Copyright (C) 2004-2015 Hampa Hug <hampa@hampa.ch>\n"
 	);
-
-	fflush (stdout);
 }
 
 static
@@ -161,46 +183,14 @@ int pce_load_config (ini_sct_t *ini, const char *fname)
 	return (0);
 }
 
-int str_isarg1 (const char *str, const char *arg)
-{
-	if (strcmp (str, arg) == 0) {
-		return (1);
-	}
-
-	return (0);
-}
-
-int str_isarg2 (const char *str, const char *arg1, const char *arg2)
-{
-	if (strcmp (str, arg1) == 0) {
-		return (1);
-	}
-
-	if (strcmp (str, arg2) == 0) {
-		return (1);
-	}
-
-	return (0);
-}
-
 int main (int argc, char *argv[])
 {
-	int       i;
+	int       r;
+	char      **optarg;
 	int       run;
 	char      *cfg;
 	ini_sct_t *sct;
 	monitor_t mon;
-
-	if (argc == 2) {
-		if (str_isarg1 (argv[1], "--help")) {
-			prt_help();
-			return (0);
-		}
-		else if (str_isarg1 (argv[1], "--version")) {
-			prt_version();
-			return (0);
-		}
-	}
 
 	cfg = NULL;
 	run = 0;
@@ -214,51 +204,84 @@ int main (int argc, char *argv[])
 		return (1);
 	}
 
-	i = 1;
-	while (i < argc) {
-		if (str_isarg2 (argv[i], "-v", "--verbose")) {
-			pce_log_set_level (stderr, MSG_DEB);
-		}
-		else if (str_isarg2 (argv[i], "-q", "--quiet")) {
-			pce_log_set_level (stderr, MSG_ERR);
-		}
-		else if (str_isarg2 (argv[i], "-c", "--config")) {
-			i += 1;
-			if (i >= argc) {
-				return (1);
-			}
-			cfg = argv[i];
-		}
-		else if (str_isarg2 (argv[i], "-l", "--log")) {
-			i += 1;
-			if (i >= argc) {
-				return (1);
-			}
-			pce_log_add_fname (argv[i], MSG_DEB);
-		}
-		else if (str_isarg2 (argv[i], "-p", "--cpu")) {
-			i += 1;
-			if (i >= argc) {
-				return (1);
-			}
+	ini_str_init (&par_ini_str);
 
-			par_cpu = argv[i];
+	while (1) {
+		r = pce_getopt (argc, argv, &optarg, opts);
+
+		if (r == GETOPT_DONE) {
+			break;
 		}
-		else if (str_isarg2 (argv[i], "-r", "--run")) {
-			run = 1;
-		}
-		else {
-			printf ("%s: unknown option (%s)\n", argv[0], argv[i]);
+
+		if (r < 0) {
 			return (1);
 		}
 
-		i += 1;
+		switch (r) {
+		case '?':
+			print_help();
+			return (0);
+
+		case 'V':
+			print_version();
+			return (0);
+
+		case 'c':
+			cfg = optarg[0];
+			break;
+
+		case 'd':
+			pce_path_set (optarg[0]);
+			break;
+
+		case 'i':
+			if (ini_read_str (par_cfg, optarg[0])) {
+				fprintf (stderr,
+					"%s: error parsing ini string (%s)\n",
+					argv[0], optarg[0]
+				);
+				return (1);
+			}
+			break;
+
+		case 'I':
+			ini_str_add (&par_ini_str, optarg[0], "\n", NULL);
+			break;
+
+		case 'l':
+			pce_log_add_fname (optarg[0], MSG_DEB);
+			break;
+
+		case 'p':
+			ini_str_add (&par_ini_str, "cpu.model = \"",
+				optarg[0], "\"\n"
+			);
+			break;
+
+		case 'q':
+			pce_log_set_level (stderr, MSG_ERR);
+			break;
+
+		case 'r':
+			run = 1;
+			break;
+
+		case 'v':
+			pce_log_set_level (stderr, MSG_DEB);
+			break;
+
+		case 0:
+			fprintf (stderr, "%s: unknown option (%s)\n",
+				argv[0], optarg[0]
+			);
+			return (1);
+
+		default:
+			return (1);
+		}
 	}
 
-	pce_log (MSG_INF,
-		"pce-sim405 version " PCE_VERSION_STR "\n"
-		"Copyright (C) 1995-2012 Hampa Hug <hampa@hampa.ch>\n"
-	);
+	s405_log_banner();
 
 	if (pce_load_config (par_cfg, cfg)) {
 		return (1);
@@ -269,6 +292,12 @@ int main (int argc, char *argv[])
 	if (sct == NULL) {
 		sct = par_cfg;
 	}
+
+	if (ini_str_eval (&par_ini_str, sct, 1)) {
+		return (1);
+	}
+
+	pce_path_ini (sct);
 
 	par_sim = s405_new (sct);
 
